baebac4354ce701cc6331414182ea55eea13ff11
Francois Lachese <francois.lachese@gmail.com>
2026-04-24 16:15:23 +0200

    add: mangowm

---
+++ .config/mango/autostart.sh
@@ -0,0 +1,7 @@
+awww-daemon &
+.config/mango/scripts/wallpaper.sh ~/Pictures/Wallpapers 3600 &
+nextcloud --background &
+waybar &
+mpd &
+mpd-mpris &
+.config/mango/scripts/lock.sh &
---
+++ .config/mango/scripts/lock.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+swayidle \
+  before-sleep 'qs -c lock' \
+	timeout 600 'wlr-dpms off' \
+	resume 'wlr-dpms on' \
+	timeout 300 'qs -c lock' \
+	resume ''
